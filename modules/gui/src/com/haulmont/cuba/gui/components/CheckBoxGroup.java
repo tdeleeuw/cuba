acab920a68e5c6a9b7e1547fe859e412db301d23
Gleb Gorelov <gorelov@haulmont.com>
2018-07-24 13:00:35 +0400

    Separate RadioButtonGroup and CheckBoxGroup components (719)
    Extract HasOrientation interface

---
+++ modules/gui/src/com/haulmont/cuba/gui/components/CheckBoxGroup.java
@@ -0,0 +1,7 @@
+package com.haulmont.cuba.gui.components;
+
+public interface CheckBoxGroup<V, I>
+        extends OptionsField<V, I>, LookupComponent, Component.Focusable, HasOrientation {
+    String NAME = "checkBoxGroup";
+
+}
---
+++ modules/gui/src/com/haulmont/cuba/gui/components/RadioButtonGroup.java
@@ -0,0 +1,7 @@
+package com.haulmont.cuba.gui.components;
+
+public interface RadioButtonGroup<V, I>
+        extends OptionsField<V, I>, LookupComponent, Component.Focusable, HasOrientation {
+    String NAME = "radioButtonGroup";
+
+}
