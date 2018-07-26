f4e102aef2c7aa93a4025f164a6877fb9b33af3e
Gleb Gorelov <gorelov@haulmont.com>
2018-07-26 19:32:44 +0400

    TreeDataGrid component (380)
    TreeDataGrid data binding

--- modules/gui/src/com/haulmont/cuba/gui/components/TreeDataGrid.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/TreeDataGrid.java
@@ -1,9 +1,39 @@
 package com.haulmont.cuba.gui.components;
 
 import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.components.data.DataGridSource;
+import com.haulmont.cuba.gui.components.data.datagrid.HierarchicalDatasourceDataGridAdapter;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.HierarchicalDatasource;
 
 public interface TreeDataGrid<E extends Entity> extends DataGrid<E> {
 
     String NAME = "treeDataGrid";
 
+    @Override
+    default HierarchicalDatasource getDatasource() {
+        DataGridSource<E> dataGridSource = getDataGridSource();
+        return dataGridSource != null ?
+                (HierarchicalDatasource) ((HierarchicalDatasourceDataGridAdapter) dataGridSource).getDatasource()
+                : null;
+    }
+
+    @Deprecated
+    default void setDatasource(HierarchicalDatasource datasource) {
+        setDatasource(datasource);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    default void setDatasource(CollectionDatasource datasource) {
+        if (datasource == null) {
+            setDataGridSource(null);
+        } else {
+            if (!(datasource instanceof HierarchicalDatasource)) {
+                throw new IllegalArgumentException("TreeDataGrid supports only HierarchicalDatasource");
+            }
+
+            setDataGridSource(new HierarchicalDatasourceDataGridAdapter((HierarchicalDatasource) datasource));
+        }
+    }
 }
--- modules/web/src/com/haulmont/cuba/web/gui/components/WebTreeDataGrid.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebTreeDataGrid.java
@@ -2,6 +2,12 @@ package com.haulmont.cuba.web.gui.components;
 
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.gui.components.TreeDataGrid;
+import com.haulmont.cuba.gui.components.data.DataGridSource;
+import com.haulmont.cuba.gui.components.data.TreeDataGridSource;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.HierarchicalDatasource;
+import com.haulmont.cuba.web.gui.components.datagrid.DataGridDataProvider;
+import com.haulmont.cuba.web.gui.components.datagrid.HierarchicalDataGridDataProvider;
 import com.haulmont.cuba.web.widgets.CubaTreeGrid;
 
 public class WebTreeDataGrid<E extends Entity> extends WebAbstractDataGrid<CubaTreeGrid<E>, E>
@@ -10,5 +16,24 @@ public class WebTreeDataGrid<E extends Entity> extends WebAbstractDataGrid<CubaT
     @Override
     protected CubaTreeGrid<E> createComponent() {
         return new CubaTreeGrid<>();
+    }
+
+    @Override
+    public void setDataGridSource(DataGridSource<E> dataGridSource) {
+        if (dataGridSource != null
+                && !(dataGridSource instanceof TreeDataGridSource)) {
+            throw new IllegalArgumentException("TreeDataGrid supports only TreeDataGridSource data binding");
+        }
+
+        super.setDataGridSource(dataGridSource);
+    }
+
+    protected TreeDataGridSource<E> getTreeDataGridSource() {
+        return (TreeDataGridSource<E>) getDataGridSource();
+    }
+
+    @Override
+    protected DataGridDataProvider<E> createDataGridDataProvider(DataGridSource<E> dataGridSource) {
+        return new HierarchicalDataGridDataProvider<>((TreeDataGridSource<E>) dataGridSource, this);
     }
 }
