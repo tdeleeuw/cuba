835305e843af24745601f4e3e842e05eb3cd88cf
Roman Pinyazhin <pinyazhin@haulmont.com>
2018-09-12 13:14:19 +0400

    Unsupported web browser page
    Support localization

--- modules/web/src/com/haulmont/cuba/web/sys/CubaUnsupportedBrowserHandler.java
+++ modules/web/src/com/haulmont/cuba/web/sys/CubaUnsupportedBrowserHandler.java
@@ -16,33 +16,61 @@
 
 package com.haulmont.cuba.web.sys;
 
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.core.global.Resources;
+import com.haulmont.cuba.core.global.TemplateHelper;
 import com.vaadin.server.*;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
 
 public class CubaUnsupportedBrowserHandler extends UnsupportedBrowserHandler {
 
-    public static final String UNSUPPORTED_PAGE_PATH = "/com/haulmont/cuba/web/sys/unsupported-page.html";
+    public static final String UNSUPPORTED_PAGE_TEMPLATE_PATH = "/com/haulmont/cuba/web/sys/unsupported-page-template.html";
 
     protected Resources resources;
+    protected Messages messages;
 
     public CubaUnsupportedBrowserHandler() {
         super();
 
         resources = AppBeans.get(Resources.NAME);
+        messages = AppBeans.get(Messages.NAME);
     }
 
     @Override
     protected void writeBrowserTooOldPage(VaadinRequest request, VaadinResponse response) throws IOException {
         try (Writer page = response.getWriter()) {
 
-            String pageContent = resources.getResourceAsString(UNSUPPORTED_PAGE_PATH);
-            if (pageContent != null) {
-                page.write(pageContent);
-            }
+            Locale locale = request.getLocale();
+
+            ParamsMap paramsMap = ParamsMap.of();
+            paramsMap.pair("captionMessage", messages.getMainMessage("unsupportedPage.captionMessage", locale));
+            paramsMap.pair("descriptionMessage", messages.getMainMessage("unsupportedPage.descriptionMessage", locale));
+            paramsMap.pair("browserListCaption", messages.getMainMessage("unsupportedPage.browserListCaption", locale));
+
+            paramsMap.pair("chromeImagePath", messages.getMainMessage("unsupportedPage.chromeImagePath"));
+            paramsMap.pair("firefoxImagePath", messages.getMainMessage("unsupportedPage.firefoxImagePath"));
+            paramsMap.pair("safariImagePath", messages.getMainMessage("unsupportedPage.safariImagePath"));
+            paramsMap.pair("operaImagePath", messages.getMainMessage("unsupportedPage.operaImagePath"));
+            paramsMap.pair("edgeImagePath", messages.getMainMessage("unsupportedPage.edgeImagePath"));
+            paramsMap.pair("explorerImagePath", messages.getMainMessage("unsupportedPage.explorerImagePath"));
+
+            paramsMap.pair("chromeMessage", messages.getMainMessage("unsupportedPage.chromeMessage", locale));
+            paramsMap.pair("firefoxMessage", messages.getMainMessage("unsupportedPage.firefoxMessage", locale));
+            paramsMap.pair("safariMessage", messages.getMainMessage("unsupportedPage.safariMessage", locale));
+            paramsMap.pair("operaMessage", messages.getMainMessage("unsupportedPage.operaMessage", locale));
+            paramsMap.pair("edgeMessage", messages.getMainMessage("unsupportedPage.edgeMessage", locale));
+            paramsMap.pair("explorerMessage", messages.getMainMessage("unsupportedPage.explorerMessage", locale));
+
+            String stringTemplate = resources.getResourceAsString(UNSUPPORTED_PAGE_TEMPLATE_PATH);
+
+            String pageContent = TemplateHelper.processTemplate(stringTemplate, paramsMap.create());
+
+            page.write(pageContent);
         }
     }
 }
